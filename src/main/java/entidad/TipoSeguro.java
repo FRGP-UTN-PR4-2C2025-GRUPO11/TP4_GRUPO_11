2fb00bd06bcaf57d1c4621fa05fb0515d717509b
CZubileteSanchez <carloszubilete@gmail.com>
2025-09-20 17:57:32 -0300

    Agregamos la clase TipoSeguro en el dominio entidad

---
+++ src/main/java/entidad/TipoSeguro.java
@@ -0,0 +1,32 @@
+package entidad;
+
+public class TipoSeguro {
+	private int id;
+	private String descripcion;
+
+	public TipoSeguro() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoSeguro [id=" + id + ", descripcion=" + descripcion + "]";
+	}
+	
+	
+}
